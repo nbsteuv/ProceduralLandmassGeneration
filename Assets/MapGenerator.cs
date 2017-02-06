9c763a2a96f3237d74eeb547204afabcadbd7b8e
nbsteuv <nbsteuv@gmail.com>
2017-02-06 05:12:23 -0500

    Create color map with land and water types

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
+    public enum DrawMode
+    {
+        NoiseMap,
+        ColorMap
+    }
+
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -20,12 +28,33 @@ public class MapGenerator : MonoBehaviour {
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight < regions[i].height)
+                    {
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+        
     }
 
     void OnValidate()
@@ -48,4 +77,13 @@ public class MapGenerator : MonoBehaviour {
         }
 
     }
+}
+
+//System.Serializable allows this to show up in inspector
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
 }

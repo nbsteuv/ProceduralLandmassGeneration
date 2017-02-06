94dff62ee0385f0b0ee69eed0cdc183e0303218d
nbsteuv <nbsteuv@gmail.com>
2017-02-05 19:49:54 -0500

    Create texture and set pixels with black/white color gradient based on noise map

--- Assets/MapDisplay.cs
+++ Assets/MapDisplay.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class MapDisplay : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Renderer textureRenderer;
+
+    public void DrawNoiseMap(float[,] noiseMap)
+    {
+        int width = noiseMap.GetLength(0);
+        int height = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        //Can use SetPixel(), but faster to generate an array and use SetPixels()
+        Color[] colorMap = new Color[width * height];
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                //Multiply y by width of the map to get row and add x to get position within row, converted to single dimension array position
+                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+            }
+            texture.SetPixels(colorMap);
+            texture.Apply();
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }

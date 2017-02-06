d3a6c94fe2a2d0258e91ea05bea5b1ed168d0cf2
nbsteuv <nbsteuv@gmail.com>
2017-02-06 04:51:35 -0500

    Added seed and octave offset values

--- Assets/Noise.cs
+++ Assets/Noise.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public static class Noise {
 
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for(int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
 
         if(scale <= 0)
         {
@@ -26,8 +35,8 @@ public static class Noise {
 
                 for(int i = 0; i < octaves; i++)
                 {
-                    float sampleX = (x / scale) * frequency;
-                    float sampleY = (y / scale) * frequency;
+                    float sampleX = (x / scale) * frequency + octaveOffsets[i].x;
+                    float sampleY = (y / scale) * frequency + octaveOffsets[i].y;
 
                     float perlinValue = (Mathf.PerlinNoise(sampleX, sampleY) * 2) - 1;
                     noiseHeight += perlinValue * amplitude;

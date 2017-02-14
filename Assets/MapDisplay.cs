4ac6750c09979eb4e65777a01b3d0c8217e2ed6e
nbsteuv <nbsteuv@gmail.com>
2017-02-14 12:17:33 -0500

    Created procedural mesh based on noise map and layered on top of plane

--- Assets/MapDisplay.cs
+++ Assets/MapDisplay.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MapDisplay : MonoBehaviour {
 
     public Renderer textureRenderer;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
 
     public void DrawTexture(Texture2D texture)
     {
@@ -13,6 +15,12 @@ public class MapDisplay : MonoBehaviour {
 
         //Scale plane to fit noisemap size
         textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
     }
 	
 }
--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -7,7 +7,8 @@ public class MapGenerator : MonoBehaviour {
     public enum DrawMode
     {
         NoiseMap,
-        ColorMap
+        ColorMap,
+        Mesh
     }
 
     public DrawMode drawMode;
@@ -59,6 +60,9 @@ public class MapGenerator : MonoBehaviour {
         } else if(drawMode == DrawMode.ColorMap)
         {
             display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        } else if(drawMode == DrawMode.Mesh)
+        {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
         }
         
     }

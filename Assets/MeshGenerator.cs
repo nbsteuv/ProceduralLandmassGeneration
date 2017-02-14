ead70867ec0ba54289cd744264b5c4ff23b7d02f
nbsteuv <nbsteuv@gmail.com>
2017-02-14 13:46:35 -0500

    Draw location of vertices based on level of detail selected in editor

--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -11,12 +11,15 @@ public static class MeshGenerator {
         float topLeftX = (width - 1) / -2f;
         float topLeftZ = (height - 1) / 2f;
 
+        int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+        int verticesPerLine = ((width - 1) / meshSimplificationIncrement) + 1;
+
         MeshData meshData = new MeshData(width, height);
         int vertexIndex = 0;
 
-        for(int y = 0; y < height; y++)
+        for(int y = 0; y < height; y += meshSimplificationIncrement)
         {
-            for(int x = 0; x < width; x++)
+            for(int x = 0; x < width; x += meshSimplificationIncrement)
             {
                 meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightCurve.Evaluate(heightMap[x, y]) * heightMultiplier, topLeftZ - y);
                 meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
@@ -24,8 +27,8 @@ public static class MeshGenerator {
                 if(x < width - 1 && y < height - 1)
                 {
                     //vertexindex + width is one row below vertexIndex in the map, converted to single-dimension array value
-                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
-                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+                    meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
+                    meshData.AddTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
                 }
 
                 vertexIndex++;

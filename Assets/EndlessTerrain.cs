f0352457bf5416c4f355c3446f5ab28b677450b2
nbsteuv <nbsteuv@gmail.com>
2017-02-18 14:45:43 -0500

    Scale terrain generation

--- Assets/EndlessTerrain.cs
+++ Assets/EndlessTerrain.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EndlessTerrain : MonoBehaviour {
 
+    const float scale = 5f;
+
     const float viewerMoveThresholdForChunkUpdate = 25f;
 
     private const float sqrViewerMoveThresholdForChunkUpdate =
@@ -34,7 +36,7 @@ public class EndlessTerrain : MonoBehaviour {
 
     void Update()
     {
-        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z) / scale;
 
         if ((viewerPositionOld - viewerPosition).sqrMagnitude > sqrViewerMoveThresholdForChunkUpdate)
         {
@@ -101,8 +103,9 @@ public class EndlessTerrain : MonoBehaviour {
             meshRenderer.material = material;
             meshFilter = meshObject.AddComponent<MeshFilter>();
 
-            meshObject.transform.position = positionV3;
+            meshObject.transform.position = positionV3 * scale;
             meshObject.transform.parent = parent;
+            meshObject.transform.localScale = Vector3.one * scale;
 
             SetVisible(false);
 

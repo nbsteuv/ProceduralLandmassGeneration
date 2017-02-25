ec19076f2e22e6cdbafb5f2b3b3732baf6020ae9
nbsteuv <nbsteuv@gmail.com>
2017-02-25 13:36:27 -0500

    Added movement controllers

---
+++ Assets/ViewController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewController : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float forwardMotion = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+		transform.Translate(Vector3.forward * forwardMotion);
+
+	    float rotation = rotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+
+	}
+}
